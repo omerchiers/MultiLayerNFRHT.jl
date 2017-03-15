66b2f63b2f71c758bc7a51e04b4fcb0d51325a97
Olivier Merchiers <olivier.merchiers@insa-lyon.fr>
2017-03-15 18:14:16 +0100

    added Bose factor, Planck's distribution and Wien's law

--- src/emissivity.jl
+++ src/emissivity.jl
@@ -2,8 +2,23 @@
 
 using Cubature # find package for integration
 
-BulkOrMultiLayer = Union{Bulk,MultiLayer}
+BulkOrMultiLayer = Union{Layer,Bulk,MultiLayer}
+LayerOrMultiLayer = Union{Layer,MultiLayer}
 
+# Factors
+function bose_einstein(w,T)
+    ħ*w/(exp(ħ*w/kb/T)-1.0)
+end
+
+function planck(w,T)
+    u = w*ħ/kb/T
+    return  u^3/(exp(u)-1.0)*kb^4*T^4/ħ^3/c0^2/(2.0*pi)^2
+end
+
+# Wien frequency : return the frequency for which Planck distribution is maximum
+function wien(T)
+    return   2.8214393721220787*kb*T/ħ
+end
 
 function emissivity_kx_w(struct :: BulkOrMultiLayer, kx, w)
 # Monocromatic emissivity
@@ -21,17 +36,12 @@ end
 
 function emissivity_w(struct :: BulkOrMultiLayer, w)
     e_kx(kx) = kx*emissivity_kx_w(struct ,kx, w)
+
     val :: Float64  = 0.0
     err :: Float64  = 0.0
     (val,err) = hquadrature(e_kx, 0.0, w/c0 ; reltol=1e-8, abstol=0, maxevals=0)
 
     return val/(w/c0)^2
-end
-
-# Total emissivity
-
-function bose_einstein(w,T)
-    ħ*w/(exp(ħ*w/kb/T)-1.0)
 end
 
 
@@ -42,6 +52,15 @@ function emissivity(struct :: BulkOrMultiLayer,T)
     val :: Float64  = 0.0
     err :: Float64  = 0.0
     (val,err) = hquadrature(e2, 0.0, 1.0 ; reltol=1e-8, abstol=0, maxevals=0)
+
+    return val*kb^4/ħ^3/c0^2/(2.0*pi)^2/sigma
+end
+
+function emissivity(struct :: BulkOrMultiLayer,T,wi,wf)
+    e(u) = emissivity_w(struct, u*kb*T/ħ)*u^3/(exp(u)-1.0)
+    val :: Float64  = 0.0
+    err :: Float64  = 0.0
+    (val,err) = hquadrature(e, wi*ħ/kb/T , wf*ħ/kb/T ; reltol=1e-8, abstol=0, maxevals=0)
 
     return val*kb^4/ħ^3/c0^2/(2.0*pi)^2/sigma
 end
