6f6ff75f2eba42f925e02d5427427775d038ca10
Olivier Merchiers <olivier.merchiers@insa-lyon.fr>
2017-03-29 15:35:07 +0200

    added export for all functionns and types

--- src/RadiativeHeat.jl
+++ src/RadiativeHeat.jl
@@ -3,7 +3,17 @@ module RadiativeHeat
 using Plots
 
 # export functions
-export bose_einstein,wien
+export bose_einstein,wien,farfield_transfer,
+       transmission_kx_w,transmission_w,
+       heat_flux,heat_flux2,heat_flux_w,
+       heat_transfer,heat_transfer_w,
+       total_heat_transfer
+
+#export types
+export OptProp,Model,
+       TotalField,Evanescent,Propagative,
+       Polarization,te,tm,
+       Structure,Bulk,Layer
 
 # export constants
 export ħ, kb, c0
