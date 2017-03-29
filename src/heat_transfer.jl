902f20d86d8e5e8e7d8c6237bc926cfa8618acac
Olivier Merchiers <olivier.merchiers@insa-lyon.fr>
2017-03-29 15:32:53 +0200

    commented the inclusion of heat _transfer_spheres.jl

--- src/heat_transfer.jl
+++ src/heat_transfer.jl
@@ -1,3 +1,8 @@
 
+abstract TotalField
+immutable Evanescent  <: TotalField end
+immutable Propagative <: TotalField end
+
+
 include("heat_transfer_plane_geometry.jl")
-include("heat_transfer_spheres.jl")
+#include("heat_transfer_spheres.jl")
