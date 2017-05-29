09507a2d12da8c41f392f0dfeb26a000a8f5c5b3
Olivier Merchiers <olivier.merchiers@insa-lyon.fr>
2017-05-29 11:55:10 +0200

    new main file that groups all heat_transfer functions

--- src/heat_transfer.jl
+++ src/heat_transfer.jl
@@ -3,6 +3,18 @@ abstract TotalField
 immutable Evanescent  <: TotalField end
 immutable Propagative <: TotalField end
 
+# abstract Spectrum
+# immutable Frequency <: Spectrum end
+# immutable Wavelength <: Spectrum end
+#
+# immutable FrequencyRange <: Spectrum
+#     w1 :: Float64
+#     w2 :: Float64
+# end
+#
+# FrequencyRange() = FrequencyRange(0.0,0.0)
+
+
 
 include("heat_transfer_plane_geometry.jl")
 #include("heat_transfer_spheres.jl")
