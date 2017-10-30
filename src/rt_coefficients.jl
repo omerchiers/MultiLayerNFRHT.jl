ec5991645286512705fef2f82039e56df993ada9
Olivier Merchiers <omerchiers@gmail.com>
2017-10-30 10:46:09 +0100

    changed to v0.6 syntax

--- src/rt_coefficients.jl
+++ src/rt_coefficients.jl
@@ -2,39 +2,35 @@
 
 #using StaticArrays
 
-abstract Polarization
-immutable te <: Polarization end
-immutable tm <: Polarization end
+abstract type Polarization end
+struct te <: Polarization end
+struct tm <: Polarization end
 
 
-abstract Structure
+abstract type Structure end
 
-immutable Bulk{T <:OptProp,U <:OptProp} <: Structure
+struct Bulk{T <:OptProp,U <:OptProp} <: Structure
     ep1 :: T
     ep2 :: U
 end
 # Outer constructor for the Bulk type when one medium is vacuum
 Bulk(ep2) = Bulk(Cst(1.0+im*0.0),ep2)
 
-immutable Layer <: Structure
+struct Layer <: Structure
     material  :: OptProp
     thickness :: Float64 # in meter. For semi-infinite media thickness = 0
 end
 Layer(material)  = Layer(material,0.0)
 
-
-#typealias MultiLayer Union{Vector{Layer},Vector{Layer{Cst}},Vector{Layer{Al}}}
-typealias MultiLayer Vector{Layer}
-
+# new Vector type
+MultiLayer = Vector{Layer}
 
 
 function compute_kz(kx,eps,w)
-
     sqrt(eps*(w/c0)^2 - kx*kx)
 end
 
 function compute_kx(theta_0,eps,w)
-
     sqrt(eps)*sin(theta_0)*w/c0
 end
 
@@ -47,7 +43,6 @@ end
 # Fresnel coefficient of a semi-infinite medium : perpendicular
 function rt(pol :: te, eps1,eps2, k0z,k2z,w)
   # k0z = incident wavevector from medium 1
-
     r = (k0z - k2z)/(k0z + k2z)
     t = (2.0+im*0.0)*k0z/(k0z + k2z)
     return r,t
@@ -56,7 +51,6 @@ end
 # Fresnel coefficient of a semi-infinite medium : parallel
 function rt(pol :: tm, eps1,eps2, k0z,k2z,w)
   # k0z = incident wavevector from medium 1
-
     r = (eps2*k0z-eps1*k2z)/(eps2*k0z+eps1*k2z)
     t = (2.0+im*0.0)*sqrt(eps2)*sqrt(eps1)*k0z/(eps2*k0z+eps1*k2z)
     return r,t
@@ -64,7 +58,6 @@ end
 
 # Fresnel coefficient of a semi-infinite medium : as a function of incidence angle and frequency
 function rt(struct :: Bulk, pol :: Polarization, kx ,w)
-
     eps1 = permittivity(struct.ep1,w)
     eps2 = permittivity(struct.ep2,w)
 
@@ -90,7 +83,6 @@ function rt(struct :: MultiLayer, pol :: Polarization, kx ,w)
 end
 
 function scattering_matrix!(S,struct :: MultiLayer, pol :: Polarization, kx, w)
-
         for i=2:length(struct)
             eps1 :: Complex128 = permittivity(struct[i-1].material,w)
             eps2 :: Complex128 = permittivity(struct[i].material,w)
@@ -104,7 +96,4 @@ function scattering_matrix!(S,struct :: MultiLayer, pol :: Polarization, kx, w)
             S[2,2] =(S[2,2]*exp(im*struct[i-1].thickness*k0z)*(r*S[1,2] + 1.0+0.0*im))/t
         end
     return S
-
-
-
 end
