7ecaa7c608837f4c56276ee0b9bc883a030001ac
Olivier Merchiers <omerchiers@gmail.com>
2018-01-17 13:35:57 +0100

    Removed commented code

--- src/heat_transfer.jl
+++ src/heat_transfer.jl
@@ -3,18 +3,5 @@ abstract type TotalField end
 struct Evanescent  <: TotalField end
 struct Propagative <: TotalField end
 
-# abstract Spectrum
-# immutable Frequency <: Spectrum end
-# immutable Wavelength <: Spectrum end
-#
-# immutable FrequencyRange <: Spectrum
-#     w1 :: Float64
-#     w2 :: Float64
-# end
-#
-# FrequencyRange() = FrequencyRange(0.0,0.0)
-
-
 
 include("heat_transfer_plane_geometry.jl")
-#include("heat_transfer_spheres.jl")
