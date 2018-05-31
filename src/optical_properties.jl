f5dd54583825d2178e17a0530e9d0d3b03f88925
Olivier Merchiers <omerchiers@gmail.com>
2018-05-31 19:51:35 +0200

    restructured around Model types. Materials are now constants of model types

--- src/optical_properties.jl
+++ src/optical_properties.jl
@@ -2,45 +2,34 @@
 # Permitivities are given in the convention : eps_real + i*eps_im
 
 export convert_prop, permittivity, epsi, refr, Drude, Lorentz, Cbn, Sic, Si, Al, Au,Au_latella,Cst
-abstract type OptProp end
 
-# Generic
-struct Model <: OptProp
-    eps0   :: Float64
-    wp     :: Float64
-    w0     :: Float64
-    gamma0 :: Float64
-    gamma1 :: Float64
+abstract type AbstractMaterial end
+abstract type AbstractModel <: AbstractMaterial end
+
+
+
+# Model types
+struct LorentzModel1{T <: Number} <: AbstractModel
+    eps0   :: T
+    wp     :: T
+    w0     :: T
+    gamma0 :: T
 end
-Model(eps0,wp,w0,gamma0) = Model(eps0,wp,w0,gamma0,0.0)
 
-# Dielectrics
-struct Sic <: OptProp end
-struct Cbn <: OptProp end
-struct Si <: OptProp end
-
-# Conductors
-struct Al <: OptProp end
-struct Au <: OptProp
-    mfp :: Float64
-    a   :: Float64
+struct LorentzModel2{T <: Number} <: AbstractModel
+    eps0   :: T
+    w_lo   :: T
+    w_to   :: T
+    gamma0 :: T
 end
-Au(mfp) = Au(mfp,1.0)
-Au() = Au(1.0,0.0)
 
-struct Au_latella{T <: Real} <: OptProp
-    mfp :: T
-    a   :: T
-end
-Au_latella(mfp) = Au_latella(mfp,1.0)
-Au_latella() = Au_latella(1.0,0.0)
+LorentzModel1(eps0,wp,w0,gamma0) = LorentzModel1(eps0,wp,w0,gamma0,0.0)
 
 # Constant permittivity
-struct Cst <: OptProp
-    val :: Complex128
+struct Cst{T <: Number} <: AbstractModel
+    val :: T
 end
 Cst() = Cst(1.0+im*0.0)
-
 
 
 """
@@ -49,7 +38,7 @@ Cst() = Cst(1.0+im*0.0)
 Compute the dielectric permittivity for a material.
 
 # Arguments
-* `material :: OptProp` : is a type for the material you want to pass
+* `material :: AbstractModel` : is a type for the material you want to pass
 * `w :: Real` : pulsation in rad s^(-1)
 
 # Example
@@ -58,58 +47,38 @@ julia> permittivity(Sic(),1e13)
 6.805820438080644 + 0.002847538251166107im
 ```
 """
-function permittivity() end
-
-
-function permittivity(material::Model,w) :: Complex128
-     eps0   = material.eps0
-     wp     = material.wp
-     w0     = material.w0
-     gamma0 = material.gamma0
-     gamma1 = material.gamma1
-    return  eps0 + wp^2/(w0*w0 - w*w - im*w*(gamma0+gamma1))
+function permittivity()
 end
 
 
-
-function permittivity(material::Cbn,w) :: Complex128
-    eps_fin = 4.46 + 0.0*im
-    w_lo    = 2.451e14 # rad/s
-    w_to    = 1.985e14 # rad/s
-    gamma   = 9.934e11 # rad/s
-    return eps_fin*(w^2-w_lo^2 + im*gamma*w)/(w^2-w_to^2 + im*gamma*w)
+function permittivity(model::LorentzModel1,w) :: Complex128
+     eps0   = model.eps0
+     wp     = model.wp
+     w0     = model.w0
+     gamma0 = model.gamma0
+     gamma1 = model.gamma1
+     return  eps0 + wp^2/(w0*w0 - w*w - im*w*(gamma0+gamma1))
 end
 
-function permittivity(material::Sic,w) :: Complex128
-    eps_fin = 6.7 + 0.0*im
-    w_lo    = 1.827e14 # rad/s
-    w_to    = 1.495e14 # rad/s
-    gamma   = 8.971e11 # rad/s
-    return eps_fin*(w^2-w_lo^2 + im*gamma*w)/(w^2-w_to^2 + im*gamma*w)
+Au(gamma1)         = LorentzModel1(9.4,13584.25e12,0.0,109.96e12,gamma1)
+Au_latella(gamma1) = LorentzModel1(1.0,1.37e16,0.0,5.32e13,gamma1)
+Al                 = LorentzModel1(1.0,2.24e16,0.0,1.22e14)
+
+function permittivity(model::LorentzModel2,w) :: Complex128
+     eps0   = model.eps0
+     w_lo   = model.wp
+     w_to   = model.w0
+     gamma0 = model.gamma0
+     return  eps0*(w^2-w_lo^2 + im*gamma*w)/(w^2-w_to^2 + im*gamma*w)
 end
 
+CbN = LorentzModel2(4.46,2.451e14,1.985e14,9.934e11)
+SiC = LorentzModel2(6.7,1.827e14,1.495e14,8.971e11)
 
-function permittivity(material::Al,w) :: Complex128
-    alum = Model(1.0,2.24e16,0.0,1.22e14)
-    return permittivity(alum,w)
-end
 
-function permittivity(material::Au,w) :: Complex128
-    gamma1 = material.a*vf_au/material.mfp
-    gold   = Model(9.4,13584.25e12,0.0,109.96e12,gamma1)
-    return permittivity(gold,w)
-end
+permittivity(model::Cst,w) = model.val
 
-function permittivity(material::Au_latella,w) :: Complex128
-    gamma1 = material.a*vf_au/material.mfp
-    gold   = Model(1.0,1.37e16,0.0,5.32e13,gamma1)
-    return permittivity(gold,w)
-end
+Si     = Cst(11.7+im*0.0)
+Vacuum = Cst(1.0+im*0.0)
 
-function permittivity(material::Si,w) :: Complex128
-    return 11.7+im*0.0
-end
-
-function permittivity(material::Cst,w) :: Complex128
-    return material.val
-end
+refractiveindex(material :: AbstractMaterial,w) = sqrt(permittivity(material,w))
