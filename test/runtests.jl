f48e0c10acd8274f593602022bd2664efa7a377d
Olivier Merchiers <omerchiers@gmail.com>
2018-09-21 17:06:55 +0200

    added test for total heat exchange between blackbodies in far field at 1 K

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,9 @@ mg = MaxwellGarnett(Cst(3.0 + 0.5*im),Cst(5.0 + 1.5*im),0.0)
 
 @test permittivity(bm,1e15) == permittivity(Cst(5.0 + 1.5*im),1e15)
 @test permittivity(mg,1e15) == permittivity(Cst(5.0 + 1.5*im),1e15)
+
+# Far-field black body heat transfer
+bb_bulk = Layer(Cst(1.0 + 1e-5*im))
+gap     = Layer(Cst(),1e-1)
+
+@test total_heat_transfer(b,b,gap,1.0,0.0,1e8,1e13) ≈ [6.17311e-14, 6.17343e-14 , 2.83518e-8 , 2.83518e-8 , 5.67038e-8 ]
