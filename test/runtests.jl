c3d6810dfb0b5cc012db35299cadd9dd5da3c7dd
Olivier Merchiers <omerchiers@gmail.com>
2018-11-20 14:23:20 +0100

    added test for SiC

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,14 +16,13 @@ gap     = Layer(Cst(),1e-1)
 
 # emissivity single bodies
 ml1 = [Layer(Cst()),Layer(Cst(1.0+1e-5*im))]
-
 @test emissivity(ml1,300.0) ≈ 0.9999999658493035
 
 ml2 = [Layer(Cst()),Layer(Au())]
-
 @test emissivity(ml2,300.0) ≈ 0.018976160729311516
 
-
 ml3 = [Layer(Cst()),Layer(Al())]
-
 @test emissivity(ml3,300.0) ≈ 0.012711414795024877
+
+ml4 = [Layer(Cst()),Layer(Sic())]
+@test emissivity(ml4,300.0) ≈ 0.6103470572275264
