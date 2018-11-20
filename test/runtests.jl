f7bacc136e95061ddab170b49ec37eb42026f321
Olivier Merchiers <omerchiers@gmail.com>
2018-11-20 14:20:50 +0100

    added test for emissivities bulk media

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,3 +13,17 @@ bb_bulk = Layer(Cst(1.0 + 1e-5*im))
 gap     = Layer(Cst(),1e-1)
 
 @test total_heat_transfer(b,b,gap,1.0,0.0,1e8,1e13) ≈ [6.17311e-14, 6.17343e-14 , 2.83518e-8 , 2.83518e-8 , 5.67038e-8 ]
+
+# emissivity single bodies
+ml1 = [Layer(Cst()),Layer(Cst(1.0+1e-5*im))]
+
+@test emissivity(ml1,300.0) ≈ 0.9999999658493035
+
+ml2 = [Layer(Cst()),Layer(Au())]
+
+@test emissivity(ml2,300.0) ≈ 0.018976160729311516
+
+
+ml3 = [Layer(Cst()),Layer(Al())]
+
+@test emissivity(ml3,300.0) ≈ 0.012711414795024877
