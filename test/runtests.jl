1551fed3ed97d1fd96403bd134e31e5c5e835277
Olivier Merchiers <omerchiers@gmail.com>
2019-06-18 11:47:55 +0200

    removed Bruggeman and MaxwellGarnett tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,6 @@
 using MultiLayerNFRHT
 using Base.Test
 
-# Effective medium models
-bm = Bruggeman(Cst(3.0 + 0.5*im),Cst(5.0 + 1.5*im),0.0)
-mg = MaxwellGarnett(Cst(3.0 + 0.5*im),Cst(5.0 + 1.5*im),0.0)
-
-@test permittivity(bm,1e15) == permittivity(Cst(5.0 + 1.5*im),1e15)
-@test permittivity(mg,1e15) == permittivity(Cst(5.0 + 1.5*im),1e15)
-
 # Far-field black body heat transfer
 bb_bulk = Layer(Cst(1.0 + 1e-5*im))
 gap     = Layer(Cst(),1e-1)
